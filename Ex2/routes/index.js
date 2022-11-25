f2074d02f5295a8544289df86fefa79d03e3d788
graviolagames <graviolagames@gmail.com>
2022-11-25 14:25:11 -0300

    until step 4

--- Ex2/routes/index.js
+++ Ex2/routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/hello/:firstName/:lastName', function(req, res, next) {
+      res.render('hello', {
+      firstName: req.params.firstName,
+      lastName: req.params.lastName
+    });
+  });
+
 module.exports = router;

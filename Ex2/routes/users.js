952bc557f3d706ae52f44b43f91e24b133aef6ea
graviolagames <graviolagames@gmail.com>
2022-11-25 20:57:26 -0300

    Step 8

--- Ex2/routes/users.js
+++ Ex2/routes/users.js
@@ -1,14 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+let users = {itens:[]};
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log("[DEBUG]- GET/users/list");
+  res.send(users.itens);
 });
 
 router.post('/', function(req, res, next) {
   console.log("[DEBUG]- POST/users");
   const body = req.body;
-  res.send(body);
+  users.itens.push(body);
+  res.send("ok");
 });
+//To test this operation use curl --request DELETE localhost:3000/users
+router.delete('/', function(req, res, next) {
+  console.log("[DEBUG]- DELETE/users");
+  let {email} = req.query;
+  let foundIndex;
+  let found = users.itens.find(function(user,index){
+    foundIndex = index;
+    return user.name == email;
+  });
+  if(found){
+    users.itens.splice(foundIndex,1);
+    return res.send("ok");
+  }
+  res.send("Not found");
+});
+
+
+
 module.exports = router;
